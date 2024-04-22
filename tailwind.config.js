2fb27699c313a77d7a3af4fe6ff24f5690cb7464
Romain Grange <romain.grange@pixlstudio.fr>
2024-04-22 11:18:42 +0200

    login admin

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.vue",
+    "./components/*.vue",
+    "./pages/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
