2328d3eda7d5566fc778e6d08dcb69077e90452b
RuddyMo <ruddymorel10@gmail.com>
2024-04-25 00:13:31 +0200

    add button

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,17 @@
   <div class="font-roboto">
     <div class="w-full h-80 bg-[url('/_nuxt/assets/images/bannerHome.png')] bg-cover flex flex-col gap-y-4 items-center justify-center text-white">
       <p class="font-redressed text-5xl">Dégustation de vin</p>
-      <div class="bg-primary py-1 px-3 cursor-pointer  rounded-md font-roboto">
-        Évenement à venir
-      </div>
+      <a href="/liste-atelier" class="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-white border-2 border-white rounded-full hover:text-white group">
+        <span class="absolute left-0 block w-full h-0 transition-all bg-primary opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+        <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="32" fill="#FFFFFF" viewBox="0 0 256 256"><path d="M245.66,42.34l-32-32a8,8,0,0,0-11.32,11.32l1.48,1.47L148.65,64.51l-38.22,7.65a8.05,8.05,0,0,0-4.09,2.18L23,157.66a24,24,0,0,0,0,33.94L64.4,233a24,24,0,0,0,33.94,0l83.32-83.31a8,8,0,0,0,2.18-4.09l7.65-38.22,41.38-55.17,1.47,1.48a8,8,0,0,0,11.32-11.32ZM96,107.31,148.69,160,104,204.69,51.31,152ZM81.37,224a7.94,7.94,0,0,1-5.65-2.34L34.34,180.28a8,8,0,0,1,0-11.31L40,163.31,92.69,216,87,221.66A8,8,0,0,1,81.37,224ZM177.6,99.2a7.92,7.92,0,0,0-1.44,3.23l-7.53,37.63L160,148.69,107.31,96l8.63-8.63,37.63-7.53a7.92,7.92,0,0,0,3.23-1.44l58.45-43.84,6.19,6.19Z"></path></svg>
+      </span>
+        <span class="relative">Liste des Ateliers</span>
+      </a>
+
+      <!--<div class="bg-primary bg-opacity-60 py-1 px-3 cursor-pointer  rounded-md font-roboto">
+        Listes des Ateliers
+      </div>-->
     </div>
     <div class="flex flex-col gap-y-20">
       <div class="flex flex-col px-6 lg:px-40">
